c91d429117e59497cf13fbf460d5040d41d78a85
Ahkar <ahkarkyaw.personal@gmail.com>
2024-02-29 10:56:14 -0800

    Initial setup.

---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+	return render(request, "portfolio/index.html")
+
+def admin(request):
+	return render(request, "portfolio/admin.html")
+
